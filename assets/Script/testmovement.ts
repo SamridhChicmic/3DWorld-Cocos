b1f56c309f75e07f09ce1a903d69d78c2da80228
SamridhChicmic <samridh.gupta@chicmic.co.in>
2023-05-01 15:13:10 +0530

    TestMovement of Character in All direction with Specific Angle

--- assets/Script/testmovement.ts
+++ assets/Script/testmovement.ts
@@ -5,6 +5,7 @@ import {
   Input,
   input,
   KeyCode,
+  lerp,
   Node,
   tween,
   Vec3,
@@ -13,13 +14,17 @@ const { ccclass, property } = _decorator;
 
 @ccclass("testmovement")
 export class testmovement extends Component {
+  @property({ type: Node })
+  character: Node = null;
   AxisArray = ["-z", "-zx", "x", "xz", "z", "z-x", "-x", "-x-z"];
   index: number = 0;
   start() {
     input.on(Input.EventType.KEY_PRESSING, this.keyBoardPressed, this);
     input.on(Input.EventType.KEY_DOWN, this.keyBoardDown, this);
+
     console.log(-1 % 180);
   }
+
   keyBoardDown(event: EventKeyboard) {
     let currentPosition: Vec3;
     switch (event.keyCode) {
@@ -31,6 +36,19 @@ export class testmovement extends Component {
         break;
       case KeyCode.ARROW_LEFT:
         let AngleLeft = this.node.eulerAngles;
+
+        // tween(this.node)
+        //  .to(0, {
+        //(
+        this.node.eulerAngles = new Vec3(
+          AngleLeft.x,
+          AngleLeft.y + 45,
+          AngleLeft.z
+        );
+        //),
+        //  })
+        // .start();
+        AngleLeft = this.node.eulerAngles;
         AngleLeft = new Vec3(AngleLeft.x, AngleLeft.y % 360, AngleLeft.z);
         if (AngleLeft.y == 180) {
           this.index = 0;
@@ -42,7 +60,7 @@ export class testmovement extends Component {
         }
         if (AngleLeft.y == 270) {
           this.index = 6;
-          AngleLeft = new Vec3(AngleLeft.x, -90, AngleLeft.z);
+          this.node.eulerAngles = new Vec3(AngleLeft.x, -90, AngleLeft.z);
           console.log("Axis", this.AxisArray[this.index]);
         }
         if (-90 < AngleLeft.y && AngleLeft.y < 0) {
@@ -65,16 +83,25 @@ export class testmovement extends Component {
           this.index = 1;
           console.log("Axis", this.AxisArray[this.index]);
         }
-        tween(this.node)
-          .to(0.5, {
-            eulerAngles: new Vec3(AngleLeft.x, AngleLeft.y + 10, AngleLeft.z),
-          })
 
-          .start();
         break;
       case KeyCode.ARROW_RIGHT:
         let AngleRight = this.node.eulerAngles;
         AngleRight = new Vec3(AngleRight.x, AngleRight.y % 360, AngleRight.z);
+        //   tween(this.node)
+        // .to(0, {
+        (this.node.eulerAngles = new Vec3(
+          AngleRight.x,
+          AngleRight.y - 45,
+          AngleRight.z
+        )),
+          //  }).start();
+          (AngleRight = this.node.eulerAngles);
+        AngleRight = new Vec3(AngleRight.x, AngleRight.y % 360, AngleRight.z);
+        if (AngleRight.y == 180) {
+          this.index = 0;
+          console.log("Axis", this.AxisArray[this.index]);
+        }
         if (180 > AngleRight.y && AngleRight.y > 90) {
           this.index = 1;
           console.log("Axis", this.AxisArray[this.index]);
@@ -97,23 +124,13 @@ export class testmovement extends Component {
         }
         if (AngleRight.y == -90) {
           this.index = 6;
-          AngleRight = new Vec3(AngleRight.x, 270, AngleRight.z);
+          this.node.eulerAngles = new Vec3(AngleRight.x, 270, AngleRight.z);
           console.log("Axis", this.AxisArray[this.index]);
         }
         if (270 > AngleRight.y && AngleRight.y > 180) {
           this.index = 7;
           console.log("Axis", this.AxisArray[this.index]);
         }
-        tween(this.node)
-          .to(0.5, {
-            eulerAngles: new Vec3(
-              AngleRight.x,
-              AngleRight.y - 10,
-              AngleRight.z
-            ),
-          })
-
-          .start();
 
         break;
     }
@@ -160,6 +177,7 @@ export class testmovement extends Component {
     if (this.AxisArray[this.index] == "-zx") {
       console.log(" Console -zx");
       currentPosition = this.node.getPosition();
+
       currentPosition.x += 0.5;
       currentPosition.z -= 0.5;
 
@@ -214,6 +232,35 @@ export class testmovement extends Component {
       currentPosition = this.node.getPosition();
       currentPosition.x += 0.5;
 
+      tween(this.node).to(0.1, { position: currentPosition }).start();
+    }
+    if (this.AxisArray[this.index] == "-zx") {
+      console.log(" Console -zx");
+      currentPosition = this.node.getPosition();
+      currentPosition.x -= 0.5;
+      currentPosition.z += 0.5;
+
+      tween(this.node).to(0.1, { position: currentPosition }).start();
+    }
+    if (this.AxisArray[this.index] == "xz") {
+      console.log(" Console xz");
+      currentPosition = this.node.getPosition();
+      currentPosition.x -= 0.5;
+      currentPosition.z -= 0.5;
+      tween(this.node).to(0.1, { position: currentPosition }).start();
+    }
+    if (this.AxisArray[this.index] == "z-x") {
+      console.log(" Console z-x");
+      currentPosition = this.node.getPosition();
+      currentPosition.x += 0.5;
+      currentPosition.z -= 0.5;
+      tween(this.node).to(0.1, { position: currentPosition }).start();
+    }
+    if (this.AxisArray[this.index] == "-x-z") {
+      console.log(" Console -x-z");
+      currentPosition = this.node.getPosition();
+      currentPosition.x += 0.5;
+      currentPosition.z += 0.5;
       tween(this.node).to(0.1, { position: currentPosition }).start();
     }
   }
