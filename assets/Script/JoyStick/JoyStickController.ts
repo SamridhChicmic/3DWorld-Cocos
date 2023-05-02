645393974c0a6c897633aebb671ebd1305cda263
SamridhChicmic <samridh.gupta@chicmic.co.in>
2023-05-02 16:39:42 +0530

    JoyStick Added With JoyStickBall Movement

--- assets/Script/JoyStick/JoyStickController.ts
+++ assets/Script/JoyStick/JoyStickController.ts
@@ -1,9 +1,67 @@
-import { _decorator, Component, Node } from "cc";
+import {
+  _decorator,
+  Component,
+  Input,
+  input,
+  Node,
+  UITransform,
+  Vec3,
+} from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("JoyStickController")
 export class JoyStickController extends Component {
-  start() {}
+  @property({ type: Node })
+  JoyStick: Node = null;
+  @property({ type: Node })
+  JoyStickBall: Node = null;
+
+  JoyStickBallBaseWidth: number = 0;
+  start() {
+    this.JoyStickBallBaseWidth =
+      this.JoyStick.getComponent(UITransform).width * 0.5;
+    console.log(this.JoyStickBallBaseWidth);
+    this.joyStickControllerTouch();
+  }
+  joyStickControllerTouch() {
+    this.node.on(Input.EventType.TOUCH_START, this.onTouchStart, this);
+    this.node.on(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
+    this.node.on(Input.EventType.TOUCH_END, this.onTouchEnd, this);
+    this.node.on(Input.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+  }
+  onTouchMove(event) {
+    let intialPos = this.JoyStick.getComponent(
+      UITransform
+    ).convertToNodeSpaceAR(
+      new Vec3(
+        event.getUILocation().x,
+        event.getUILocation().y,
+        event.getUILocation().z
+      )
+    );
+
+    if (
+      intialPos.y <= this.JoyStickBallBaseWidth &&
+      intialPos.x <= this.JoyStickBallBaseWidth &&
+      intialPos.y >= -this.JoyStickBallBaseWidth &&
+      intialPos.x >= -this.JoyStickBallBaseWidth
+    ) {
+      if (intialPos.y > 0) {
+        console.log("Runnforword");
+      } else if (intialPos.y < 0) {
+        console.log("backword");
+      }
+      this.JoyStickBall.setPosition(intialPos);
+    }
+  }
+  onTouchStart() {
+    console.log("TouchStart");
+    this.JoyStickBall.setPosition(0, 0, 0);
+  }
+  onTouchEnd() {
+    console.log("TouchEnd");
+    this.JoyStickBall.setPosition(0, 0, 0);
+  }
   onTouch() {
     console.log("Hello");
   }
